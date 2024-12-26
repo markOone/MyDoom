cc7125b9f9efd9fd25b2a4d68642095a54301d1b
markOone <98772113+markOone@users.noreply.github.com>
2024-12-26 17:22:06 +0200

    0.0.7
    -Added GameManager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance { get { if (_instance == null) Debug.Log("No GameManager"); return _instance; } }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
