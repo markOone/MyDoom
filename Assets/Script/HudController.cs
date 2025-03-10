c9a8c5c16b74c0de5aaa07ce8e2685e771c4bc92
markOone <98772113+markOone@users.noreply.github.com>
2025-03-10 11:36:53 +0200

    0.3.3+

--- Assets/Script/HudController.cs
+++ Assets/Script/HudController.cs
@@ -28,19 +28,7 @@ public class HudController : MonoBehaviour
         _instance = this;
         HUD = this.gameObject;
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void UpdateHUD()
     {
         
